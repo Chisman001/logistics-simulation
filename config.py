33f9bdb25b9c00529e587004dfce68df5763d105
Emmanuel Chisom <chismanemmans@gmail.com>
2026-07-02 21:26:57 +0100

    Initial project structure and simulation configuration

---
+++ config.py
@@ -0,0 +1,25 @@
+
+class Config:
+  # Tank
+  TANK_CAPACITY = 520
+  TANK_DURATION = 8 * 60      # minutes
+
+  # Filling
+  FILL_TIME = 2 * 60
+
+  # Travel
+  TRAVEL_TIME = 90
+
+  # Safety
+  SAFETY_WINDOW = 60
+
+  # Working hours
+  WORK_START = 6 * 60
+  WORK_END = 18 * 60
+
+  # Resources
+  NUM_TANKS = 4
+  NUM_TRUCK_HEADS = 2
+
+  # Simulation
+  SIMULATION_DAYS = 30
